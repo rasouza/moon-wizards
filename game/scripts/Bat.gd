46ed55895107f56b69ddd8e279c757c2895e0f29
Éderson Cássio <ederson_cassio@yahoo.com.br>
2017-10-10 11:00:22 -0300

    Colisão do morcego com o mago

--- game/scripts/Bat.gd
+++ game/scripts/Bat.gd
@@ -10,13 +10,26 @@ const ABAIXO = 4
 
 var direcao = NENHUMA
 
+var boxBat = null
+var boxWizard = null
+
 onready var wizard = get_parent().get_node("Wizard")
 onready var animacao = get_node("AnimationPlayer")
 
 func _ready():
 	self.set_process(true)
+	boxBat = RectangleShape2D.new()
+	boxWizard = RectangleShape2D.new()
+	var tamBat = self.get_region_rect().size
+	var tamWizard = wizard.get_region_rect().size
+	boxBat.set_extents(Vector2(tamBat.width, tamBat.height))
+	boxWizard.set_extents(Vector2(tamWizard.width, tamWizard.height))
 
 func _process(delta):
+	if (boxBat.collide(get_transform(), boxWizard, wizard.get_transform())):
+		# por enquanto, não fazemos nada :)
+		return
+	
 	var velocidade = wizard.get_pos() - self.get_pos()
 	velocidade = velocidade.normalized()
 	self.set_pos(self.get_pos() + velocidade*delta*VEL_BAT)
