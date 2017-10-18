1f372fc81acb0fc5acc16a273ddaa0becbd60a2c
Éderson Cássio <ederson_cassio@yahoo.com.br>
2017-10-18 21:50:12 -0200

    Mago com diferentes tipos de ataques com baixo acoplamento

--- game/scripts/Attack1.gd
+++ game/scripts/Attack1.gd
@@ -6,9 +6,6 @@ func _ready():
 	._ready()
 	dano = 10
 	set_z(-10)
-	
+
 func attack_frame():
-	var wizard_pos = wizard.get_global_pos()
-	var mouse = get_viewport().get_mouse_pos()
-	var attack_dir = (mouse - wizard_pos).normalized()
 	set_pos(attack_dir*distancia)
