82ddc8e86f1430f1e0425af59849efeed3e3d69b
Éderson Cássio <ederson_cassio@yahoo.com.br>
2017-10-18 21:22:07 -0200

    Inserindo barreira no mago

--- game/scripts/Barrier.gd
+++ game/scripts/Barrier.gd
@@ -1,10 +1,4 @@
-extends Node
+extends "GeneralAttack.gd"
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+func attack_frame():
 	pass
