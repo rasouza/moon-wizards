a69fde2de5d72e97e4ac11b257ade303f5c155ae
R. A. Souza <alves.wm@gmail.com>
2017-10-25 20:49:38 -0200

    Burn ataca todos os inimigos que estão no seu range

--- game/scripts/attacks/AttackBurn.gd
+++ game/scripts/attacks/AttackBurn.gd
@@ -6,7 +6,25 @@ const DAMAGE = 5
 
 ######################
 
+var enemies = {}
+var counter = 0
+
+func _ready():
+	get_node("Area2D").connect("body_exit", self, "_on_body_exit") # Colisor
+
 func attack_loop(delta):
 	set_pos(attack_dir * RANGE)
+	counter += delta
+	if (counter >= 1 and not enemies.empty()):
+		counter = 0
+		for i in enemies:
+			enemies[i].HP -= DAMAGE
+			enemies[i].get_hit(self)
+	
+func hit( enemy ):
+	enemies[enemy.get_name()] = enemy
+
+func _on_body_exit( body ):
+	enemies.erase(body.get_name())
 
 
