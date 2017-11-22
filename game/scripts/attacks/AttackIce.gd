e9962f4940b5c9feb2aba4bce31c05b8d7850722
Éderson Cássio <ederson_cassio@yahoo.com.br>
2017-11-22 17:07:06 -0200

    Sons do gelo - prepração e disparo

--- game/scripts/attacks/AttackIce.gd
+++ game/scripts/attacks/AttackIce.gd
@@ -1,16 +1,25 @@
 extends Object
 
 const AttackIceSprite = preload("res://game/sprites/attacks/AttackIceSprite.tscn")
+const IcePrepareSound = preload("res://game/sound/ice-prepare.smp")
+const IceShotSound = preload("res://game/sound/ice.smp")
 
 const INTERVAL_WAIT = 2
 const INTERVAL_SHOT = 0.3
 
 var wizard
 var timer
+var sound_library
+var sample_player
 
 func _init(wizard):
 	self.wizard = wizard
 	self.timer = Timer.new()
+	sound_library = SampleLibrary.new()
+	sound_library.add_sample("prepare", IcePrepareSound)
+	sound_library.add_sample("shot", IceShotSound)
+	sample_player = SamplePlayer.new()
+	sample_player.set_sample_library(sound_library)
 
 func attack():
 	wizard.add_child(timer)
@@ -18,6 +27,7 @@ func attack():
 	timer.set_wait_time(INTERVAL_WAIT)
 	timer.connect("timeout", self, "apos_espera")
 	timer.start()
+	sample_player.play("prepare")
 
 func apos_espera():
 	timer.disconnect("timeout", self, "apos_espera")
@@ -37,7 +47,7 @@ func disparo():
 	ice.wizard = wizard
 	ice.ref_dir = get_attack_dir()
 	wizard.get_parent().add_child(ice)
-	ice.sound.play("ice")
+	sample_player.play("shot")
 
 func get_attack_dir(): 
 	var mouse = wizard.get_global_mouse_pos()
