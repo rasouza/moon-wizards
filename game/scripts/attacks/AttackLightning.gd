fa12f4b2decbfd5b2291c82af78600f90f190c3f
Éderson Cássio <ederson_cassio@yahoo.com.br>
2017-11-23 08:49:33 -0200

    Finalizando a colisão do raio

--- game/scripts/attacks/AttackLightning.gd
+++ game/scripts/attacks/AttackLightning.gd
@@ -3,33 +3,22 @@ extends KinematicBody2D
 const TYPE = preload("../types.gd")
 const INTERVAL_WAIT = 2
 const INTERVAL_SHOT = 1
-const DAMAGE = 20
-
-const PARADO = 1
-const CARREGANDO = 2
-const ATACANDO = 3
+const DAMAGE = 10
 
 onready var anim = get_node("Sprite/Animation")
 onready var sound = get_node("SamplePlayer")
+var type = TYPE.ATTACK
 var timer
-var estado = PARADO
+var atacando = false
 var enemies = {}
 
-#func _init(wizard):
-#	._init(wizard)
-#	
-#	self.wizard = wizard
-
 func _ready():
+	set_fixed_process(true)
 	timer = Timer.new()
-	get_node("Area2D").connect("body_exit", self, "_on_body_enter")
+	get_node("Area2D").connect("body_enter", self, "_on_body_enter")
 	get_node("Area2D").connect("body_exit", self, "_on_body_exit")
 	
 func attack():
-	
-	print("Carregando raio")
-	
-	estado = CARREGANDO
 	enemies = {}
 	add_child(timer)
 	timer.set_one_shot(true)
@@ -39,16 +28,13 @@ func attack():
 	sound.play("prepare")
 
 func comecar_ataque():
-	
-	print("Raio carregado")
-	
 	var mouse = get_parent().get_global_mouse_pos()
 	var wizard_pos = get_parent().get_global_pos()
 	var attack_dir = (mouse - wizard_pos).normalized()
 	var angulo = atan2(attack_dir.x, attack_dir.y) - 1.5
 	set_rot(angulo)
-	
-	estado = ATACANDO
+
+	atacando = true
 	timer.disconnect("timeout", self, "comecar_ataque")
 	timer.set_one_shot(true)
 	timer.set_wait_time(INTERVAL_SHOT)
@@ -59,6 +45,7 @@ func comecar_ataque():
 	sound.play("thunder")
 
 func parar_ataque(): 
+	atacando = false
 	timer.disconnect("timeout", self, "parar_ataque")
 	hide()
 	anim.stop()
@@ -70,23 +57,28 @@ func stop():
 	remove_child(timer)
 	hide()
 	anim.stop()
-	estado = PARADO
+	atacando = false
 
-func _fixed_process(delta):
-	if (estado != ATACANDO): pass
+func _fixed_process(delta):	
+	if (!atacando): return
+	
 	if (not enemies.empty()):
 		for i in enemies:
+			print("Atingindo: " + enemies[i].get_name())
 			enemies[i].HP -= DAMAGE
 			enemies[i].get_hit(self)
 
 func _on_body_enter(body):
-	if (estado != ATACANDO): pass
+	
+	print("Entrou: " + body.get_name())
+	
 	if (body.type == TYPE.ENEMY):
 		enemies[body.get_name()] = body
-		print("Atingiu: " + body.get_name())
+		
 
 func _on_body_exit(body):
-	if (estado != ATACANDO): pass
+	
+	print("Saiu: " + body.get_name())
+	
 	if (body.type == TYPE.ENEMY):
 		enemies.erase(body.get_name())
-		print("Liberou: " + body.get_name())
