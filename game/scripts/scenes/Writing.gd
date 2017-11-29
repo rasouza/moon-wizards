a01cda72aa6a27badf682d2af4ad18ee18d62102
Luiz <luizfilipetm@uol.com.br>
2017-11-29 19:34:55 -0200

    fiz a tela de controles ficar mais legal

--- game/scripts/scenes/Writing.gd
+++ game/scripts/scenes/Writing.gd
@@ -3,7 +3,6 @@ extends Panel
 func _ready():
 	pass
 
-<<<<<<< HEAD
 func _on_HATE_button_pressed():
 	Transition.humor = "HATE"
 	Transition.fade_to("res://game/scenes/Chosen_Writing.tscn")
@@ -19,7 +18,3 @@ func _on_LOGIC_button_pressed():
 func _on_POWER_button_pressed():
 	Transition.humor = "POWER"
 	Transition.fade_to("res://game/scenes/Chosen_Writing.tscn")
-=======
-func _on_PLAY_button_pressed():
-	Transition.fade_to("res://game/scenes/Splash_screen.tscn")
->>>>>>> 9a7a23ac06aa6bc3e80186d994bbb9a16dd888d4
