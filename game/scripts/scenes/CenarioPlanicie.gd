277b166239b384b5ecac02f83db2ad5a8a2a2880
christianmiyoshi <christianmiyoshi@hotmail.com>
2017-11-29 19:29:12 -0200

    Alteracoes nas transicoes

--- game/scripts/scenes/CenarioPlanicie.gd
+++ game/scripts/scenes/CenarioPlanicie.gd
@@ -8,5 +8,9 @@ func _ready():
 	
 	get_node("TextIndicator").show_text_list(show_list)
 	
+	var path = "res://game/scenes/Floresta.tscn"
 	
-	get_node("TilesObjects/Book").scene_path = "res://game/scenes/Floresta.tscn"
+	get_node("TilesObjects/Book").scene_path = path
+	get_node("TilesObjects/Book 2").scene_path = path
+	get_node("TilesObjects/Book 3").scene_path = path
+	get_node("TilesObjects/Book 4").scene_path = path
